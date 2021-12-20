81b84260b03718c0245d429b2aa9e4ebd95edeea
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2021-12-20 22:23:55 +0530

    edit welcome component(home page)

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,20 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 import { useNavigate } from "react-router-dom";
+import BackgroundImage from "./assets/background_image.png";
+import Logo from "./assets/logo.png";
 const Welcome = () => {
   const navigate = useNavigate();
   return (
     <Box
       sx={{
+        align: "center",
+        backgroundImage: `linear-gradient(rgba(127,0,0,0.75),rgba(0,0,127,0.75)), url(${BackgroundImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "repeat",
         width: "100%",
         height: "100%",
         display: "flex",
@@ -15,10 +23,32 @@ const Welcome = () => {
         alignItems: "center",
       }}
     >
-      <Typography sx={{ m: 2 }} align="center" variant="h5">
+      <AppBar position="fixed">
+        <Toolbar sx={{ backgroundColor: "#F7F7F7" }}>
+          <Box
+            component="img"
+            sx={{
+              height: 64,
+              mx: 2,
+            }}
+            alt="MITRA Logo"
+            src={Logo}
+          />
+          <Typography
+            variant="h6"
+            component="h1"
+            sx={{ color: "black", fontWeight: "bold" }}
+          >
+            MITRA Learning App
+          </Typography>
+          {/* <Box component="img" src={Logo} /> */}
+        </Toolbar>
+      </AppBar>
+      <Typography sx={{ m: 2, color: "white" }} align="center" variant="h5">
         Hone you technical skills and ace in your career!
       </Typography>
       <Button
+        color="primary"
         sx={{ m: 2 }}
         variant="contained"
         onClick={() => {
