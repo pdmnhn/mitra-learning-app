13d3e8597137102dfa387e729e613b3af2a8d27c
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2022-01-29 20:07:27 +0530

    firebase init

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID,
+  apiKey: "AIzaSyD4YH9Fc6ZOPuBjrjgeVAlavwwVnEH3cCc",
+  authDomain: "mitra-learning.firebaseapp.com",
+  projectId: "mitra-learning",
+  storageBucket: "mitra-learning.appspot.com",
+  messagingSenderId: "204199760096",
+  appId: "1:204199760096:web:407a3924f193118e024085",
+  measurementId: "G-248JVPL736",
 };
 
 // Initialize Firebase
