64584fed8a5d67a58b73938cf2c9c60789d2fea4
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2022-01-30 21:13:30 +0530

    add firebase util functions

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,11 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getPerformance } from "firebase/performance";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD4YH9Fc6ZOPuBjrjgeVAlavwwVnEH3cCc",
   authDomain: "mitra-learning.firebaseapp.com",
@@ -16,6 +23,72 @@ const firebaseConfig = {
   measurementId: "G-248JVPL736",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const analytics = getAnalytics(app);
+const performance = getPerformance(app);
+
+const handleError = (errorCode) => {
+  switch (errorCode) {
+    case "auth/email-already-in-use": {
+      alert("The email address is already in use by another account.");
+      break;
+    }
+    case "auth/user-not-found": {
+      alert("Account with that email doesn't exists");
+      break;
+    }
+    case "auth/weak-password": {
+      alert("Password should be at least 6 characters.");
+      break;
+    }
+    case "auth/invalid-email": {
+      alert("Invalid email");
+      break;
+    }
+    case "auth/wrong-password": {
+      alert("The password is invalid.");
+      break;
+    }
+    default: {
+      alert(errorCode);
+    }
+  }
+};
+
+const signIn = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    return true;
+  } catch (err) {
+    handleError(err.code);
+    return false;
+  }
+};
+
+const signUp = async (email, password) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    return true;
+  } catch (err) {
+    handleError(err.code);
+    return false;
+  }
+};
+
+const resetPassword = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+    return true;
+  } catch (err) {
+    handleError(err.code);
+    return false;
+  }
+};
+
+const logOut = () => {
+  signOut(auth);
+};
+
+export { auth, signIn, signUp, resetPassword, logOut };
